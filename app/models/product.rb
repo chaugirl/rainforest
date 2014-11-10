ee6a24aa53921f0d20438e690289e388f0698438
chaugirl <chau.marialuisa@gmail.com>
2014-11-10 16:57:25 -0500

    Writing product validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,6 @@
 class Product < ActiveRecord::Base
+	# When you create a product, both the description and name have to be present
+	validates :description, :name, presence: true
+	# When you create a product, the price_in_cents must be an integer
+	validates :price_in_cents, numericality: {only_integer: true}
 end
