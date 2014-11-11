1db35deb44ace106bdd2bddfdf9383fb8ad9d47e
chaugirl <chau.marialuisa@gmail.com>
2014-11-11 16:48:52 -0500

    Writing the review routes, controllers and views

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ActiveRecord::Base
+	has_many :reviews
+	has_many :users, through: :reviews
 	# When you create a product, both the description and name have to be present
 	validates :description, :name, presence: true
 	# When you create a product, the price_in_cents must be an integer
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rainforest::Application.routes.draw do
-  resources :products
+  resources :products do
+    resources :reviews, only: [:show, :create, :destroy]
+  end
+    
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
